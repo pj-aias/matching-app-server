4783cab692c9a7805fc03be6a53515bd54ef5a08
kino-ma <kino.ma.ms@gmail.com>
2021-08-10 22:48:21 +0900

    [feat] read MySQL Password from Docker Secrets

--- api/main.go
+++ api/main.go
@@ -1,7 +1,14 @@
 package main
 
 import (
+	"log"
+	"os"
+
 	"github.com/gin-gonic/gin"
+)
+
+var (
+	mysql_password string
 )
 
 func main() {
@@ -12,4 +19,25 @@ func main() {
 		})
 	})
 	r.Run() // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
+}
+
+func init() {
+	f, err := os.Open("/run/secrets/mysql_password")
+	if err != nil {
+		log.Fatal("failed to open MySQL Password: ", err)
+	}
+
+	defer f.Close()
+
+	buf := make([]byte, 128)
+	n, err := f.Read(buf)
+
+	if n == 0 {
+		log.Fatal("MySQL Password file empty")
+	}
+	if err != nil {
+		log.Fatal("failed to read MySQL Password: ", err)
+	}
+
+	mysql_password = string(buf)
 }
