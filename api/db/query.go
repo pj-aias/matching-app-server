2e9897cac0916c5c2f15cdb47ae6dbef096e4a87
kino-ma <kino.ma.ms@gmail.com>
2021-08-15 20:04:38 +0900

    [fix] compile error

--- api/db/query.go
+++ api/db/query.go
@@ -1,11 +1,9 @@
 package db
 
-import "gorm.io/gorm"
-
 func GetUser(id uint64) (User, error) {
-	user := User{
-		gorm.Model{ID: uint(id)},
-	}
+	user := User{}
+	user.ID = uint(id)
+
 	result := database.Take(&user)
 
 	return user, result.Error
