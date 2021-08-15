7eb435f792c854a9efce112123b8cb875758942e
kino-ma <kino.ma.ms@gmail.com>
2021-08-15 22:14:34 +0900

    [feat] add `PasswordHash` model

--- api/db/models.go
+++ api/db/models.go
@@ -11,6 +11,13 @@ type User struct {
 	Bio      string `gorm:"size:255"`
 }
 
+type PasswordHash struct {
+	ID   int
+	Hash []byte `gorm:"size:72"`
+	User User   `gorm:"constraint:OnUpdate:CASCADE,OnDelete:SET NULL;"`
+}
+
 func autoMigrate(database *gorm.DB) {
 	database.AutoMigrate(&User{})
+	database.AutoMigrate(&PasswordHash{})
 }
