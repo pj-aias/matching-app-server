91badf32298b57a649e40c6cf423d3e6a99a630e
kino-ma <kino.ma.ms@gmail.com>
2021-08-15 19:57:49 +0900

    [fix] return error of `GetUser` correctly

--- api/db/query.go
+++ api/db/query.go
@@ -6,7 +6,7 @@ func GetUser(id uint64) (User, error) {
 	user := User{
 		gorm.Model{ID: uint(id)},
 	}
-	database.Take(&user)
+	result := database.Take(&user)
 
-	return user, database.Error
+	return user, result.Error
 }
