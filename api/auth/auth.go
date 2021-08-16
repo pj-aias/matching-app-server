6dbd4bce301a9b44224110f4ba20d256cb1d441d
kino-ma <kino.ma.ms@gmail.com>
2021-08-16 09:11:46 +0900

    [feat] read or generate key pair to sign jwt

--- api/auth/auth.go
+++ api/auth/auth.go
@@ -1,10 +1,31 @@
 package auth
 
 import (
+	"crypto/rand"
+	"crypto/rsa"
+	"crypto/x509"
+	"encoding/pem"
 	"errors"
+	"fmt"
+	"io/fs"
+	"io/ioutil"
+	"log"
+	"os"
+	"time"
 
+	jwt "github.com/dgrijalva/jwt-go"
 	"golang.org/x/crypto/bcrypt"
 )
+
+var (
+	privKey *rsa.PrivateKey
+	PubKey  *rsa.PublicKey
+)
+
+type CustomClaims struct {
+	UserId int
+	jwt.StandardClaims
+}
 
 func GeneratePasswordHash(password string) ([]byte, error) {
 	// bcrypt won't work correctly if the password length is > 72
@@ -23,4 +44,150 @@ func GeneratePasswordHash(password string) ([]byte, error) {
 
 func Validate(hash []byte, password string) error {
 	return bcrypt.CompareHashAndPassword(hash, []byte(password))
+}
+
+func CreateToken(user_id int) string {
+	claims := CustomClaims{
+		user_id,
+		jwt.StandardClaims{
+			ExpiresAt: time.Now().Add(time.Minute * 60).Unix(),
+			Issuer:    "makino",
+		},
+	}
+
+	token := jwt.NewWithClaims(jwt.SigningMethodRS512, claims)
+	signed_string, err := token.SignedString(sign_key)
+	if err != nil {
+		panic(err)
+	}
+
+	return signed_string
+}
+
+func VerifyUser(token_value string) (user_id int) {
+	token, err := jwt.Parse(token_value, func(token *jwt.Token) (interface{}, error) {
+		if _, ok := token.Method.(*jwt.SigningMethodRSA); !ok {
+			return nil, fmt.Errorf("Unexpected signing method: %v", token.Header["alg"])
+		}
+
+		return verify_key, nil
+	})
+
+	if err != nil {
+		panic(err)
+	}
+
+	if claims, ok := token.Claims.(jwt.MapClaims); ok && token.Valid {
+		// All claim values seems to be float. So we must convert to int after assertion to float
+		user_id = int(claims["UserId"].(float64))
+		log.Printf("authorization complete for user %v", user_id)
+	} else {
+		panic("Get user_id failed")
+	}
+
+	return
+}
+
+func readPrivateKey() error {
+	privKeyPath := os.Getenv("SECRET_KEY")
+	pubKeyPath := os.Getenv("PUBLIC_KEY")
+	privKeyFile, err := os.Open(privKeyPath)
+
+	if err == fs.ErrNotExist {
+		// if not generated key pair yet
+		privKeyFile, err = os.Create(privKeyPath)
+		if err != nil {
+			return err
+		}
+		pubKeyFile, err := os.Create(pubKeyPath)
+		if err != nil {
+			return err
+		}
+
+		privKey, PubKey, err = generateKeyPair(privKeyFile, pubKeyFile)
+
+		return err
+	} else if err != nil {
+		// normal error
+		return err
+	}
+
+	// if opened successfuly
+
+	privKeyBuf, err := ioutil.ReadAll(privKeyFile)
+	pubKeyBuf, err := ioutil.ReadAll(pubKeyFile)
+
+	privKey, err = jwt.ParseRSAPrivateKeyFromPEM(privKeyBuf)
+	if err != nil {
+		return err
+	}
+
+	PubKey, err = jwt.ParseRSAPublicKeyFromPEM(pubKeyBuf)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func generateKeyPair(privKeyFile, pubKeyFile *os.File) (*rsa.PrivateKey, *rsa.PublicKey, error) {
+	privateKey, err := rsa.GenerateKey(rand.Reader, 2048)
+	publicKey := &privateKey.PublicKey
+
+	privKeyBytes := x509.MarshalPKCS1PrivateKey(privateKey)
+	privKeyBlock := &pem.Block{
+		Type:  "RSA PRIVATE KEY",
+		Bytes: privKeyBytes,
+	}
+	err = pem.Encode(privKeyFile, privKeyBlock)
+	if err != nil {
+		return nil, nil, err
+	}
+
+	pubKeyBytes := x509.MarshalPKCS1PrivateKey(publicKey)
+	if err != nil {
+		return nil, nil, err
+	}
+	pubKeyBlock := &pem.Block{
+		Type:  "PUBLIC KEY",
+		Bytes: pubKeyBytes,
+	}
+	err = pem.Encode(pubKeyFile, pubKeyBlock)
+	if err != nil {
+		return nil, nil, err
+	}
+
+	return privateKey, publicKey, nil
+
+}
+
+func init() {
+	privKeyPath := os.Getenv("SECRET_KEY")
+	privKeyFile, err := os.Open(privKeyPath)
+
+	if err == fs.ErrNotExist {
+		privKeyFile, err = os.Create(privKeyPath)
+		rsa.GenerateKey(rand.Reader, 2048)
+	}
+
+	if err != nil {
+
+	} else {
+
+	}
+
+	sign_key, err = jwt.ParseRSAPrivateKeyFromPEM(secret_key)
+	if err != nil {
+		panic(err)
+	}
+
+	public_key, err := ioutil.ReadFile(os.Getenv("PUBLIC_KEY_PKCS8"))
+	if err != nil {
+		panic(err)
+	}
+
+	verify_key, err = jwt.ParseRSAPublicKeyFromPEM(public_key)
+	if err != nil {
+		panic(err)
+	}
 }
--- api/go.mod
+++ api/go.mod
@@ -3,6 +3,7 @@ module github.com/pj-aias/matching-app-server
 go 1.16
 
 require (
+	github.com/dgrijalva/jwt-go v3.2.0+incompatible
 	github.com/gin-gonic/gin v1.7.3
 	github.com/go-sql-driver/mysql v1.6.0
 	golang.org/x/crypto v0.0.0-20200622213623-75b288015ac9
