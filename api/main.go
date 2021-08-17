bd2b7b24c297763ec42ec4efec815670d6e14e12
kino-ma <kino.ma.ms@gmail.com>
2021-08-17 11:04:20 +0900

    [feat] use `AuthorizeToken` as a middleware

--- api/main.go
+++ api/main.go
@@ -4,14 +4,20 @@ import (
 	"github.com/gin-gonic/gin"
 
 	"github.com/pj-aias/matching-app-server/controller"
+	"github.com/pj-aias/matching-app-server/controller/middleware"
 )
 
 func main() {
 	r := gin.Default()
 
-	r.GET("user/:id", controller.UserShow)
-	r.POST("user", controller.UserAdd)
-	r.PATCH("user", controller.UserUpdate)
+	authRequired := r.Group("/")
+	authRequired.Use(middleware.AuthorizeToken())
+
+	{
+		r.GET("user/:id", controller.UserShow)
+		r.POST("user", controller.UserAdd)
+		r.PATCH("user", controller.UserUpdate)
+	}
 
 	r.Run() // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
 }
