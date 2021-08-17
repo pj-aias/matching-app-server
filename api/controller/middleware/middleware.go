e1435d0b26aaa8682d50f45e4bfab8268f15817d
kino-ma <kino.ma.ms@gmail.com>
2021-08-17 11:39:38 +0900

    [feat] test that `Authorizaton` header starts with `"Bearer"`

--- api/controller/middleware/middleware.go
+++ api/controller/middleware/middleware.go
@@ -2,6 +2,7 @@ package middleware
 
 import (
 	"net/http"
+	"strings"
 
 	"github.com/gin-gonic/gin"
 	"github.com/pj-aias/matching-app-server/auth"
@@ -11,10 +12,17 @@ func AuthorizeToken() gin.HandlerFunc {
 	return func(c *gin.Context) {
 		const BEARER_SCHEMA = "Bearer"
 		authHeader := c.GetHeader("Authorization")
+
+		if !strings.HasPrefix(authHeader, BEARER_SCHEMA) {
+			c.AbortWithStatus(http.StatusUnauthorized)
+			return
+		}
+
 		tokenString := authHeader[len(BEARER_SCHEMA):]
 		userId, err := auth.ValidateToken(tokenString)
 		if err != nil {
 			c.AbortWithStatus(http.StatusUnauthorized)
+			return
 		}
 
 		c.Set("userId", userId)
