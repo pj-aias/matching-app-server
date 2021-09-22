eabac7721baf9b9808d336a4add07ddade0805f5
kino-ma <kino.ma.ms@gmail.com>
2021-09-22 18:39:11 +0900

    [feat] update encoding to line-separeted

--- api/auth/distributed_bbs_test.go
+++ api/auth/distributed_bbs_test.go
@@ -1,7 +1,6 @@
 package auth
 
 import (
-	"encoding/base64"
 	"testing"
 
 	"github.com/google/go-cmp/cmp"
@@ -35,22 +34,16 @@ func TestFromData(t *testing.T) {
 }
 
 func TestEncode(t *testing.T) {
-	// generated from Rust
-	wanted, err := base64.StdEncoding.DecodeString("k5tzb21lTWVzc2FnZa1zb21lU2lnbmF0dXJlp3NvbWVHcGs=")
-	if err != nil {
-		t.Fatalf("failed to decode sample base64")
-	}
+	wanted := []byte(`someSignature
+someGpk
+someMessage`)
 
 	params := VerifyParams{
 		Message:   []byte("someMessage"),
 		Signature: "someSignature",
 		Gpk:       "someGpk",
 	}
-	got, err := params.encode()
-
-	if err != nil {
-		t.Fatalf("auth.VerifyParams.encode() failed; err = %v", err)
-	}
+	got := params.encode()
 
 	if !cmp.Equal(got, wanted) {
 		t.Errorf("invalid result: wanted %v, got %v", wanted, got)
