be439ac70b812bdc7ea1c5278e4fff50afd51ab0
kino-ma <kino.ma.ms@gmail.com>
2021-09-23 02:27:21 +0900

    [feat] add signature valification to middleware

--- api/controller/middleware/middleware.go
+++ api/controller/middleware/middleware.go
@@ -1,13 +1,17 @@
 package middleware
 
 import (
+	"bytes"
 	"fmt"
+	"io/ioutil"
 	"net/http"
 	"os"
 	"strings"
 
 	"github.com/gin-gonic/gin"
+
 	"github.com/pj-aias/matching-app-server/auth"
+	"github.com/pj-aias/matching-app-server/db"
 )
 
 func AuthorizeToken() gin.HandlerFunc {
@@ -30,6 +34,34 @@ func AuthorizeToken() gin.HandlerFunc {
 		}
 
 		c.Set("userId", userId)
+
+		// validate signature
+		// FIXME: extract to another midldeware
+		signature := c.GetHeader("AnoMatch-Signature")
+		gpk, err := db.GetUserGpk(userId)
+		if err != nil {
+			c.AbortWithStatusJSON(http.StatusInternalServerError, gin.H{"error": "could not find GPK"})
+			return
+		}
+
+		body, err := ioutil.ReadAll(c.Request.Body)
+		if err != nil {
+			c.AbortWithStatus(http.StatusUnauthorized)
+			return
+		}
+
+		// body (Reader) will be consumed if once read, so reset the data
+		c.Request.Body = ioutil.NopCloser(bytes.NewBuffer(body))
+		ok, err := auth.VerifySignature(body, signature, gpk)
+		if err != nil {
+			c.AbortWithStatusJSON(http.StatusInternalServerError, gin.H{"error": "failed to validate signature"})
+			return
+		}
+		if !ok {
+			c.AbortWithStatusJSON(http.StatusBadRequest, gin.H{"error": "invalid signature"})
+			return
+		}
+
 		c.Next()
 	}
 }
