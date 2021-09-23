38fc4fc9d0afea339935c9da8963ee945cc91db8
kino-ma <kino.ma.ms@gmail.com>
2021-09-23 22:00:43 +0900

    [feat] define tpes to receive gpk [wip]

--- api/auth/distributed_bbs/gpk.go
+++ api/auth/distributed_bbs/gpk.go
@@ -1,18 +1,29 @@
 package distributed_bbs
 
-import "fmt"
+import (
+	"fmt"
+	"sort"
+)
 
 type GpkRegistry interface {
 	GetGpk(gm Gm) (Gpk, error)
 }
+
+type Gms []Gm
+
+func (gms Gms) Len() int           { return len(gms) }
+func (gms Gms) Less(i, j int) bool { return gms[i] < gms[j] }
+func (gms Gms) Swap(i, j int)      { gms[i], gms[j] = gms[j], gms[i] }
 
 func CombineGpk(gpks []Gpk) (Gpk, error) {
 	// TODO implement CpmbineGpk process
 	return gpks[0], nil
 }
 
-func GetGpkFromGms(registry GpkRegistry, gms []Gm) (Gpk, error) {
+func GetGpkFromGms(registry GpkRegistry, gms Gms) (Gpk, error) {
 	gpks := []Gpk{}
+	sort.Sort(gms)
+
 	for _, gm := range gms {
 		gpk, err := registry.GetGpk(gm)
 		if err != nil {
@@ -45,3 +56,13 @@ func (r CachedRegistry) Get(gm Gm) (Gpk, error) {
 
 	return gpk, nil
 }
+
+type fp [6]uint64
+
+type GpkResp struct {
+	H fp `json:"h"`
+	U fp `json:"u"`
+	V fp `json:"v"`
+}
+
+func sendRequest(gm Gm)
