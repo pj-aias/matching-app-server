c7a246a00ad42d6983f0446b0ca026973bdcc434
kino-ma <kino.ma.ms@gmail.com>
2021-09-24 01:52:39 +0900

    [feat] add `util` package, containing helpers about tor

--- api/go.mod
+++ api/go.mod
@@ -9,6 +9,7 @@ require (
 	github.com/google/go-cmp v0.5.6
 	github.com/vmihailenco/msgpack/v5 v5.3.4
 	golang.org/x/crypto v0.0.0-20200622213623-75b288015ac9
+	golang.org/x/net v0.0.0-20210917221730-978cfadd31cf // indirect
 	gorm.io/driver/mysql v1.1.2
 	gorm.io/gorm v1.21.13
 )
---
+++ api/util/tor.go
@@ -0,0 +1,24 @@
+package util
+
+import (
+	"fmt"
+	"net/http"
+
+	"golang.org/x/net/proxy"
+)
+
+type TorClient *http.Client
+
+func NewTorClient() (TorClient, error) {
+	p, err := proxy.SOCKS5("tcp", "tor:9050", nil, proxy.Direct)
+	if err != nil {
+		return nil, fmt.Errorf("failed to open Tor proxy: %v", err)
+	}
+
+	client := http.DefaultClient
+	client.Transport = &http.Transport{
+		Dial: p.Dial,
+	}
+
+	return client, nil
+}
